60651b7a68ec84c76870f966396aab35c76065bb
Alexandre <alex_didi49@hotmail.com>
2020-01-23 10:07:39 +0100

    :ambulance: wii u wii u wii u
    resolving twig issue

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+Class HomeController extends AbstractController{
+    
+    /**
+     * @Route("",name="home_index",methods={"GET"})
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {   
+        
+        return $this->render('Home/index.html.twig');
+    }
+
+}
+
