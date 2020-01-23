a0b2e4e127b5272d3728eb72d9e706be59e75bcc
MOREAU Eliott <moreaueliott49@gmail.com>
2020-01-23 15:06:54 +0100

    :construction_worker: Eliott form builder

---
+++ src/Form/AdType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+                'label' => 'Titre',
+                'attr' => ['placeholder' => 'Titre...', 'maxlength' => 100]
+            ])
+            ->add('description',TextType::class, [
+                'required' => true,
+                'label' => 'Description',
+                'attr' => ['placeholder' => 'Description de votre annonce...']
+            ])
+            ->add('city',TextType::class, [
+                'required' => true,
+                'label' => 'Ville',
+                'attr' => ['placeholder' => 'Ville...', 'maxlength' => 70]
+            ])
+            ->add('zip',IntegerType::class, [
+                'required' => true,
+                'label' => 'Code postal',
+                'attr' => ['placeholder' => 'Code postal...', 'maxlength' => 5]
+            ])
+            ->add('price', MoneyType::class, [
+                'required' => true,
+                'label' => 'Prix',
+                'attr' => ['placeholder' => 'Prix...']
+            ])
+            ->add('datecreated',TextType::class, [
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+            'trim' => true,
+        ]);
+    }
+}
